8d5097c9d50c141b9ac01597af51701d1d5f17e0
MarilenaCretu <marilenac2001@gmail.com>
2022-11-13 22:16:43 +0200

    Initial commit

---
+++ src/main/java/ExercitiulCalculator/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+//        LogicalOp logicalOp=new LogicalOp();
+//
+//        int biggest = logicalOp.checkBiggerNumber(2,3);
+//        System.out.println("The bigger number is: " + biggest);
+//
+//        System.out.println(logicalOp.verificaretext("FastTrackIT", logicalOp.second));
+//        System.out.println(logicalOp.verificare("FastTrack", 2));
+//        System.out.println(logicalOp.maimareoptsauegalsase(9));
+//        System.out.println(logicalOp.maimaretreisauegalpatru(3));
+//        LogicalOp.numberswich();
+//        System.out.println(logicalOp.isNumberEven(18));
+//        System.out.println(logicalOp.isEligibleToVote(17));
+//        System.out.println(logicalOp.maimare(77,2,55));
+
+
+
+        Calculator calculator=new Calculator();
+
+        System.out.println(calculator.getExercitiulunu());
+        System.out.println(calculator.getAfisarecerintadoi());
+        System.out.println(calculator.getPrimmulnumar());
+        System.out.println(calculator.getAldoileanumar());
+        System.out.println(calculator.getExercitiuldoi());
+
+        System.out.println(calculator.getExercitiultrei());
+
+        System.out.println(calculator.getCalculA());
+        System.out.println(calculator.getCalculB());
+        System.out.println(calculator.getCalculC());
+        System.out.println(calculator.getCalculD());
+
+        System.out.println(calculator.getSuma());
+        System.out.println(calculator.getScaderea());
+        System.out.println(calculator.getInmultirea());
+        System.out.println(calculator.getImpartirea());
+
+        System.out.println(calculator.getJava());
+        System.out.println(calculator.getMedia());
+        System.out.println(calculator.getModelfata());
+        System.out.println(calculator.getRestulimpartirii());
+        System.out.println(calculator.getTemperatura());
+        System.out.println(calculator.getMetri());
+        System.out.println(calculator.getDistanta());
+    }
+}
