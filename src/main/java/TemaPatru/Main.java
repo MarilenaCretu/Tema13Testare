687a93e564173af197d0e29d230257208315de18
MarilenaCretu <marilenac2001@gmail.com>
2022-11-14 08:11:21 +0200

    Commit

--- src/main/java/TemaPatru/Main.java
+++ src/main/java/TemaPatru/Main.java
@@ -1,5 +1,7 @@
 package TemaPatru;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         LogicalOp logicalOp = new LogicalOp();
@@ -11,7 +13,16 @@ public class Main {
         logicalOp.minim(array);
         System.out.print("Cel mai mare numar din array este: ");
         logicalOp.maxim(array);
-
+        System.out.println("\nExercitiul 3:\n\tCreati o metoda care sa inverseze valorile unui array de int-uri:");
+        System.out.print("Array initial: ");
+        logicalOp.printArrayValue(array);
+        Arrays.sort(array,2,4);
+        System.out.println("\nInversare Array:" + Arrays.toString(array));
+        System.out.println("\nExercitiul 6:\n\tCreati o metoda care sa primeasca un array de numere ne-ordonat, si sa il returneze ordonat crescator:");
+        System.out.print("Array initial: ");
+        logicalOp.printArrayValue(array);
+        Arrays.sort(array);
+        System.out.println("\nOrdonare Array:" + Arrays.toString(array));
 
     }
 
