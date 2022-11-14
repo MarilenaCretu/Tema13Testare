d539a068cfe5a85b9d448751db645c5ee65586e9
MarilenaCretu <marilenac2001@gmail.com>
2022-11-14 06:55:59 +0200

    Commit

--- src/main/java/TemaPatru/LogicalOp.java
+++ src/main/java/TemaPatru/LogicalOp.java
@@ -2,31 +2,35 @@ package TemaPatru;
 
 public class LogicalOp {
 
+    //Exercitiul 1
     public void printArrayValue(int[] myArray) {
         for (int i = 0; i < myArray.length; i++) {
             System.out.print(myArray[i] + " ");
         }
     }
 
-    public int min(int[] array) {
-        int min = array[0];
+    //Exercitiul 2
+    public int minim(int[] array) {
+        int minim = array[0];
         for (int i = 0; i < array.length; i++) {
-            if (array[i] < min) {
-                min = array[i];
-                System.out.println(min);
+            if (array[i] < minim) {
+                minim = array[i];
+                System.out.println(minim);
             }
         }
-        return min;
+        return minim;
     }
 
-    public int max(int[] array) {
-        int max = array[0];
+    public int maxim(int[] array) {
+        int maxim = array[0];
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > max) {
-                max = array[i];
+            if (array[i] > maxim) {
+                maxim = array[i];
             }
         }
-        System.out.println(max);
-        return max;
+        System.out.println(maxim);
+        return maxim;
     }
+
+
 }
--- src/main/java/TemaPatru/Main.java
+++ src/main/java/TemaPatru/Main.java
@@ -1,16 +1,16 @@
 package TemaPatru;
 
-
 public class Main {
     public static void main(String[] args) {
         LogicalOp logicalOp = new LogicalOp();
-        System.out.println("\nExercitiul 2:\n\tCreati o metoda care sa gaseasca cel mai mare si cel mai mic numar dintr-un array:");
+        System.out.println("Exercitiul 1:\n\tCreati o metoda care insereze un element pe o pozitie specifica intr-un array:");
         int [] array = new int [] {56, 3, 90, 5, 98};
         logicalOp.printArrayValue(array);
-        System.out.print("\nCel mai mic numar din array este: ");
-        logicalOp.min(array);
+        System.out.println("\n\nExercitiul 2:\n\tCreati o metoda care sa gaseasca cel mai mare si cel mai mic numar dintr-un array:");
+        System.out.print("Cel mai mic numar din array este: ");
+        logicalOp.minim(array);
         System.out.print("Cel mai mare numar din array este: ");
-        logicalOp.max(array);
+        logicalOp.maxim(array);
 
 
     }
